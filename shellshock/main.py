4d75884840fca7a83f9bd96537f521e6c2a434c9
mattdodge <matt@n.io>
2019-01-08 22:20:04 -0800

    Add flag to include original source

--- shellshock/main.py
+++ shellshock/main.py
@@ -2,4 +2,4 @@ from shellshock.parse import convert_source
 import sys
 
 if __name__ == '__main__':
-    print(convert_source(sys.argv[1]))
+    print(convert_source(sys.argv[1], include_source=True))
