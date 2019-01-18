cea27620ab083addf1d9a08f2840a3b765875669
mattdodge <matt@n.io>
2019-01-18 10:40:30 -0800

    Add setup.py and python gitignore

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="shellshock",
+    version="0.0.1",
+    author="Matt Dodge",
+    author_email="mattedgod@gmail.com",
+    description="Write your shell scripts using Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mattdodge/shellshock",
+    packages=setuptools.find_packages(),
+)
