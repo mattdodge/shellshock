444a8744de54bc6b6ff3ff5fc0add8d0cdcfb518
mattdodge <matt@n.io>
2019-01-22 21:45:35 -0800

    Add deploy step to circleci

--- setup.py
+++ setup.py
@@ -1,11 +1,30 @@
+import os
 import setuptools
+from setuptools.command.install import install
+import sys
+
+VERSION = '0.0.1'
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            sys.exit(
+                "Git tag: {0} does not match the version of "
+                "this app: {1}".format(tag, VERSION))
+
+
 setuptools.setup(
     name="shellshock",
-    version="0.0.1",
+    version=VERSION,
     author="Matt Dodge",
     author_email="mattedgod@gmail.com",
     description="Write your shell scripts using Python",
@@ -17,5 +36,8 @@ setuptools.setup(
         'console_scripts': [
             'shellshock=shellshock.main:main',
         ],
+    },
+    cmdclass={
+        'verify': VerifyVersionCommand,
     },
 )
